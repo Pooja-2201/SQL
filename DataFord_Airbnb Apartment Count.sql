09d197dacddf048b1c335fcdd04d4b0db51a7b0c
Pooja-2201 <134257337+Pooja-2201@users.noreply.github.com>
2024-07-30 16:00:46 +0530

    Update and rename DataFord_Airbnb Apartment Count to DataFord_Airbnb Apartment Count.sql

---
+++ DataFord_Airbnb Apartment Count.sql
@@ -0,0 +1,9 @@
+-- https://dataford.io/sql/Airbnb-Apartment-Count
+Solution:
+SELECT 
+    h.nationality AS NATIONALITY,
+    COUNT(u.unit_id) AS APARTMENTS_COUNT
+FROM airbnb_hosts h
+INNER JOIN airbnb_units u ON h.host_id = u.host_id
+WHERE h.age < 30
+GROUP BY h.nationality;
