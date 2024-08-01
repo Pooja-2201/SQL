3ce41768c7d9d6fba59395339a0674efb771d314
Pooja-2201 <134257337+Pooja-2201@users.noreply.github.com>
2024-08-01 11:34:47 +0530

    Create DataFord_Listening-habits.sql

---
+++ DataFord_Listening-habits.sql
@@ -0,0 +1,6 @@
+--https://dataford.io/sql/Listening-habits
+SELECT user_id,
+    ROUND(SUM(listen_duration) / 60.0) AS total_listen_duration,
+    COUNT(DISTINCT song_id) AS unique_song_count
+FROM listening_habits
+GROUP BY user_id;
